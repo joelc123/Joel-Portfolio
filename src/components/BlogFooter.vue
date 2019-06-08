bcbc0eb47dff299805999818d8d9be0ba60cbb05
joelc123 <32792370+joelc123@users.noreply.github.com>
2019-06-08 09:46:44 +0100

    update

--- src/components/BlogFooter.vue
+++ src/components/BlogFooter.vue
@@ -1,6 +1,10 @@
 <template>
   <transition appear name="v--mask">
     <footer v-once class="blog__footer">
+
+      <a href="https://cosmicjs.com/add-bucket?import_bucket=5cfb6f40b8b13e72d10df46c"><img src="https://cdn.cosmicjs.com/51fe54d0-4f6e-11e9-9f32-8d001da69630-powered-by-cosmicjs.svg" /></a>
+
+
       <div class="signature cosmicjs">
         Proudly powered by <a href="https://cosmicjs.com" target="_blank">Cosmic JS</a>
       </div>
